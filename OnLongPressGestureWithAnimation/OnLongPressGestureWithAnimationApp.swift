5ca5d9e8f6ec3c25b5ee710a47af2afb36a25fde
DjNovovic <djordjijenovovic@gmail.com>
2021-08-24 17:38:55 +0200

    Initial Commit

---
+++ OnLongPressGestureWithAnimation/OnLongPressGestureWithAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnLongPressGestureWithAnimationApp.swift
+//  OnLongPressGestureWithAnimation
+//
+//  Created by Đorđije Novović on 24.8.21..
+//
+
+import SwiftUI
+
+@main
+struct OnLongPressGestureWithAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
